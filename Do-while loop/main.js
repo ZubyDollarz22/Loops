f839bf4e74bc75a174d85fd5cf4d75a9dffbc49a
ZubyDollarz <zubydollarz22@gmail.com>
2024-11-12 04:03:52 +0100

    Do-while loop

---
+++ Do-while loop/main.js
@@ -0,0 +1,9 @@
+//A do...while statement says to do a task once and then keep doing it until a specified condition is no longer met. 
+
+let cupsOfSugarNeeded = 20;
+let cupsAdded = 0;
+
+do {
+ cupsAdded++
+ console.log(cupsAdded + ' cup was added') 
+} while (cupsAdded < cupsOfSugarNeeded);
